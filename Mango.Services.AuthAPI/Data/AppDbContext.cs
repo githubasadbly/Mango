b1a69c2681d5a85c84a18b33f86b96cbc27bfce5
dell <ansariasadbly@gmail.com>
2024-08-04 01:47:32 +0530

    4SectionComplted

---
+++ Mango.Services.AuthAPI/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Mango.Services.AuthAPI.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.AuthAPI.Data
+{
+    // pahle isse table generate krte hai  IdentityDbContext<IdentityUser> defualt user hai 
+    // also change in program.cs me
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
